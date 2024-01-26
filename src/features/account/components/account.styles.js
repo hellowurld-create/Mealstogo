b7fe2a75bfc093bb701133ff3b892a6b28c8a1dc
hellowurld-create <madebyaugus@gmail.com>
2024-01-26 01:33:56 +0100

    finished auth screen

--- src/features/account/components/account.styles.js
+++ src/features/account/components/account.styles.js
@@ -5,7 +5,7 @@ import styled from "styled-components";
 export const AccountCover = styled.View`
     position: absolute;
     width: 100%;
-    height: 90%;
+    height: 100%;
     background-color: rgba(255, 255, 255, 0.8); 
     border-top-left-radius: 20px;
     border-top-right-radius: 20px;
@@ -17,55 +17,51 @@ export const AccountCoverInner = styled.View`
     flex: 0.8;
     justify-content: center;
     align-items: center;
-    margin-top: 70px;
     background-color: white;
 `;
 
 export const CoverTitle = styled.Text`
-    font-size: 40px;
+    font-size: 30px;
     font-weight: bold;
     color: #333;
-    padding: 10px; 
+    padding: 5px; 
     text-align: center; 
-    letter-spacing: 1px; /* Adjust as needed */
+    letter-spacing: 1px; 
     text-transform: capitalize; 
     line-height: 48px; 
-    text-shadow: 2px 2px 2px rgba(0, 0, 0, 0.1); /* Add text shadow for depth */
+    text-shadow: 2px 2px 2px rgba(0, 0, 0, 0.1); 
 `;
 
 export const CoverSubtitle = styled.Text`
     font-size: 16px;
     color: #666; 
     text-align: center; 
-    margin-bottom: 20px; /* Adjust as needed */
+    margin-bottom: 20px; 
 `;
 
 export const CoverImage = styled.Image`
     width: 350px;
-    height: 350px;
-    flex:1;
+    height: auto;
+    flex:0.7;
     align-items: center;
     justify-content: center;
-    margin-top:70px;
+    margin-top:150px;
 `;
 
 export const ButtonContainer = styled.View`
-    margin-top: 0px;
     width: 100%;
 `;
 
 export const CustomButton = styled.TouchableOpacity`
-    padding: 25px 30px; 
+    background-color: #FEB500;
+    padding: 20px 30px; 
     border-radius: 5px;
     align-items: center;
     margin: 8px 10px;
-    border-width: 2px;
-    border-color: #007bff; /* Set border color */
-     color: #666;
 `;
 
 export const CustomButtonText = styled.Text`
-    color: #666;
+    color: #fff;
     font-size: 18px;
     font-weight: bold;
     letter-spacing: 1px; 
--- src/features/account/screens/account.screen.js
+++ src/features/account/screens/account.screen.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import { Button } from 'react-native'; // Assuming you have a button component available
 import res from '../../../../assets/res.png';
-import { AccountBackground, AccountCover, AccountCoverInner, ButtonContainer, CoverImage, CoverSubtitle, CoverTitle, CustomButton } from '../components/account.styles';
+import { AccountCover, AccountCoverInner, ButtonContainer, CoverImage, CoverSubtitle, CoverTitle, CustomButton, CustomButtonText } from '../components/account.styles';
 
 export const AccountScreen = () => {
     return (
             <AccountCover>
                   <CoverImage source={res} />
                 <AccountCoverInner>
-                    <CoverTitle>Welcome to SharpMeals</CoverTitle>
-                    <CoverSubtitle>Manage your profile and settings</CoverSubtitle>
+                    <CoverTitle>Welcome to Sharp Meals</CoverTitle>
+                    <CoverSubtitle>Get the best locations for restaurants.</CoverSubtitle>
                     <ButtonContainer>
-                        <CustomButton title="Login" onPress={() => console.log("Login button pressed")} />
-                        <CustomButton title="Register" onPress={() => console.log("Register button pressed")} />
+                    <CustomButton
+                        icon="lock-open-outline"
+                        mode="contained"
+                        onPress={() => console.log("Login button pressed")}>
+                        <CustomButtonText>Login</CustomButtonText>
+                    </CustomButton>
+                    <CustomButton onPress={() => console.log("Register button pressed")}>
+                        <CustomButtonText>Register</CustomButtonText>
+                    </CustomButton>
                     </ButtonContainer>
                 </AccountCoverInner>
             </AccountCover>
